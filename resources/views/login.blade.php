311739cb245f247c1dc61a603b2301e810e4c918
sofiasfmld <sfmldmaulida9@gmail.com>
2024-06-24 08:34:16 +0700

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,8 +9,7 @@
           <div class="card" style="border-radius: 1rem;">
             <div class="row g-0">
               <div class="col-md-6 col-lg-5 d-none d-md-block">
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
-                  alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+                  <img src="main/img/coba.jpg" alt="login form" class="img-fluid full-height" style="border-radius: 1rem 0 0 1rem; object-fit: cover;" />
               </div>
               <div class="col-md-6 col-lg-7 d-flex align-items-center">
                 <div class="card-body p-4 p-lg-5 text-black">
@@ -40,8 +39,7 @@
                         @csrf
     
                         <div class="d-flex align-items-center mb-3 pb-1">
-                        <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
-                        <span class="h1 fw-bold mb-0">Logo</span>
+                        <img src="main/img/logo.png" alt="Hairnic Logo" class="img-fluid" style="width: 250px; height: auto;">
                         </div>
     
                         <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
@@ -75,5 +73,11 @@
       </div>
     </div>
   </section>
+
+  <style>
+    .full-height {
+        height: 100%;
+    }
+</style>
 
 @endsection
